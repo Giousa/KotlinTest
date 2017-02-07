c222cde0f2898117fca0cf793ec778fdb6d3c960
Giousa <65489469@qq.com>
2017-02-07 23:38:44 +0800

    first commit

---
+++ app/src/main/java/com/giousa/kotlintest/Main.kt
@@ -0,0 +1,18 @@
+package com.giousa.kotlintest
+
+/**
+ *
+ * Description:
+ * Author:zhangmengmeng
+ * Date:2017/2/7
+ * Time:下午11:06
+ *
+ */
+
+fun main(args : Array<out String>){
+    println("hello world!")
+    println(Main(2,"sss"))
+}
+
+data class Main(val id: Int,val name: String)
+
